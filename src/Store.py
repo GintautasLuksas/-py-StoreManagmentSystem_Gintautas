4c7718749a474008beee241c75b303d553aa45f8
GintautasLuksas <167891054+GintautasLuksas@users.noreply.github.com>
2024-06-01 16:56:21 +0300

    Update Store.py

--- src/Store.py
+++ src/Store.py
@@ -1,20 +1,55 @@
+from Person import Worker, Manager, StoreManager
 class Store:
     def __init__(self, store_name: str, worker: list, managers: list, store_manager: str):
         self.store_name = store_name
         self.worker = worker
         self.managers = managers
         self.store_manager = store_manager
-        self.stock = []
+        self.stock = ['deb', 'dab']
 
     def show_people(self):
-        print(f'''
-In this store are working:
-{self.worker}
-{self.managers}
-{self.store_manager}.''')
+            worker_list = '\n'.join(self.worker)
+            managers_list = '\n'.join(self.managers)
+            print(f'''
+    In this store are working:
+    Workers:
+    {worker_list}
+
+    Managers:
+    {managers_list}
+
+    Store Manager:
+    {self.store_manager}
+    ''')
     def show_stock(self):
-        print()
+        print('Currently we have these items in stock:')
         for items in self.stock:
-            print('Currently we have these items in stock:')
             print(items)
 
+
+    def load_item_list(self, filename: str):
+        with open(filename, 'r') as file:
+            for line in file:
+                item_details = line.strip().split(',')
+                if len(item_details) == 4:
+                    name, price, amount, item_code = item_details
+                    self.stock.append({
+                        'name': name,
+                        'price': price,
+                        'amount': amount,
+                        'item_code': item_code
+                    })
+
+    def remove_item_list(self):
+        self.stock = []
+        with open('product_list.txt', 'w') as file:
+            file.truncate()
+
+    def add_worker(self, worker: Worker):
+        self.workers.append(worker)
+
+    def add_manager(self, manager: Manager):
+        self.managers.append(manager)
+
+    def set_store_manager(self, store_manager: StoreManager):
+        self.store_manager = store_manager
